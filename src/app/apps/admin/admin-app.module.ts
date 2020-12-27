5f3a7740e7b5283372d5d240cdcb7e086b9bec87
Michael Roma <mike@romacode.com>
2020-12-27 16:35:55 -0500

    delete obsolete admin objects

--- src/app/apps/admin/admin-app.module.ts
+++ src/app/apps/admin/admin-app.module.ts
@@ -1,50 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminHomePageComponent } from './pages/admin-home-page/admin-home-page.component';
-import { AdminPermissionsEditPageComponent } from './pages/permissions/admin-permissions-edit-page/admin-permissions-edit-page.component';
-import { AdminPermissionsListPageComponent } from './pages/permissions/admin-permissions-list-page/admin-permissions-list-page.component';
-import { AdminPermissionsListComponent } from './components/permissions/admin-permissions-list/admin-permissions-list.component';
-import { AdminPermissionsEditComponent } from './components/permissions/admin-permissions-edit/admin-permissions-edit.component';
 import { AdminContainerComponent } from './containers/admin-container/admin-container.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminNavComponent } from './components/shared/admin-nav/admin-nav.component';
-import { PermissionsContextService } from './services/permissions/permissions-context.service';
-import { AdminPermissionsContainerComponent } from './containers/admin-permissions-container/admin-permissions-container.component';
 import { FormsModule } from '@angular/forms';
-import { AdminListItemContainerComponent } from './containers/admin-list-item-container/admin-list-item-container.component';
-import { AdminListItemListComponent } from './components/list-items/admin-list-item-list/admin-list-item-list.component';
-import { AdminListItemEditComponent } from './components/list-items/admin-list-item-edit/admin-list-item-edit.component';
-import { AdminListItemEditPageComponent } from './pages/list-items/admin-list-item-edit-page/admin-list-item-edit-page.component';
-import { AdminListItemListPageComponent } from './pages/list-items/admin-list-item-list-page/admin-list-item-list-page.component';
 import { AppCommonModule } from 'src/app/common/app-common.module';
-import { AdminWorkflowGroupContainerComponent } from './containers/admin-workflow-group-container/admin-workflow-group-container.component';
-import { AdminWorkflowGroupListPageComponent } from './pages/workflow-groups/admin-workflow-group-list-page/admin-workflow-group-list-page.component';
-import { AdminWorkflowGroupEditPageComponent } from './pages/workflow-groups/admin-workflow-group-edit-page/admin-workflow-group-edit-page.component';
-import { AdminWorkflowGroupEditComponent } from './components/workflow-groups/admin-workflow-group-edit/admin-workflow-group-edit.component';
-import { AdminWorkflowGroupListComponent } from './components/workflow-groups/admin-workflow-group-list/admin-workflow-group-list.component';
-import { AdminWorkflowGroupApiService } from './services/workflow-groups/admin-workflow-group-api.service';
 import { AdminMenuItemsService } from './services/admin-menu-items.service';
 
 @NgModule({
   declarations: [
     AdminHomePageComponent,
-    AdminPermissionsEditPageComponent, 
-    AdminPermissionsListPageComponent, 
-    AdminPermissionsListComponent, 
-    AdminPermissionsEditComponent, 
     AdminContainerComponent, 
     AdminNavComponent, 
-    AdminPermissionsContainerComponent, 
-    AdminListItemContainerComponent, 
-    AdminListItemListComponent, 
-    AdminListItemEditComponent, 
-    AdminListItemEditPageComponent, 
-    AdminListItemListPageComponent, 
-    AdminWorkflowGroupContainerComponent, 
-    AdminWorkflowGroupListPageComponent, 
-    AdminWorkflowGroupEditPageComponent,     
-    AdminWorkflowGroupEditComponent, 
-    AdminWorkflowGroupListComponent
   ],
   imports: [
     CommonModule,
@@ -53,8 +21,6 @@ import { AdminMenuItemsService } from './services/admin-menu-items.service';
     FormsModule
   ],
   providers: [
-    PermissionsContextService,
-    AdminWorkflowGroupApiService,
     AdminMenuItemsService
   ]
 })
--- src/app/apps/admin/admin-routing.module.ts
+++ src/app/apps/admin/admin-routing.module.ts
@@ -3,15 +3,6 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminContainerComponent } from './containers/admin-container/admin-container.component';
 import { AdminHomePageComponent } from './pages/admin-home-page/admin-home-page.component';
-import { AdminPermissionsListPageComponent } from './pages/permissions/admin-permissions-list-page/admin-permissions-list-page.component';
-import { AdminPermissionsEditPageComponent } from './pages/permissions/admin-permissions-edit-page/admin-permissions-edit-page.component';
-import { AdminPermissionsContainerComponent } from './containers/admin-permissions-container/admin-permissions-container.component';
-import { AdminListItemContainerComponent } from './containers/admin-list-item-container/admin-list-item-container.component';
-import { AdminListItemListPageComponent } from './pages/list-items/admin-list-item-list-page/admin-list-item-list-page.component';
-import { AdminListItemEditPageComponent } from './pages/list-items/admin-list-item-edit-page/admin-list-item-edit-page.component';
-import { AdminWorkflowGroupContainerComponent } from './containers/admin-workflow-group-container/admin-workflow-group-container.component';
-import { AdminWorkflowGroupListPageComponent } from './pages/workflow-groups/admin-workflow-group-list-page/admin-workflow-group-list-page.component';
-import { AdminWorkflowGroupEditPageComponent } from './pages/workflow-groups/admin-workflow-group-edit-page/admin-workflow-group-edit-page.component';
 
 // routing
 const routes: Routes = [
@@ -21,15 +12,6 @@ const routes: Routes = [
       children: [
           { path: '', component: AdminHomePageComponent },
           
-          { 
-            path: 'permissions', 
-            component: AdminPermissionsContainerComponent,
-            children: [
-              { path: '', component: AdminPermissionsListPageComponent },
-              { path: 'edit/:key', component: AdminPermissionsEditPageComponent },
-              { path: 'edit', redirectTo: '' }
-            ]
-          },
 
           {
             path: 'security-roles',        
