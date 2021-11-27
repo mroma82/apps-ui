f4a4674169db20c41398c25c1907fecff25464a7
Michael <mike@romacode.com>
2021-11-27 10:17:36 -0500

    pm badges for overdue

--- src/app/apps/pm/modules/activities/services/pm-activity-listing-configuration.service.ts
+++ src/app/apps/pm/modules/activities/services/pm-activity-listing-configuration.service.ts
@@ -10,18 +10,18 @@ export class PmActivityListingConfigurationService implements IEntityListingConf
   // views
   getViews(): Observable<IEntityListingView[]> {
     return of([
-      { 
+      {
         id: "all",
         title: "All Activities",
-        filter: { },
+        filter: {},
         sort: {
           field: "description"
         }
       },
-      { 
+      {
         id: "overdue",
         title: "Overdue Activities",
-        filter: { 
+        filter: {
           isOverdue: true
         },
         sort: {
@@ -36,7 +36,20 @@ export class PmActivityListingConfigurationService implements IEntityListingConf
     return of([
       { model: "pmItem.description", title: "Item", isLink: true, viewLinkFunc: x => `/app/preventative-maintenance/items/view/${x.itemId}` },
       { model: "description", title: "Description", isLink: true, showEditLink: true },
-      { model: "schedulingDescription", title: "Schedule" }
+      { model: "schedulingDescription", title: "Schedule" },
+      { model: "extras.isOverdue", title: "Status", displayFunc: x => this.getStatusBadge(x) }
     ]);
+  }
+
+  // get status badge
+  getStatusBadge(activityItem: any): string {
+
+    // check if overdue
+    if (activityItem.extras.isOverdue) {
+      return `<span class="badge badge-warning">Overdue</span>`;
+    }
+
+    // all else, nothing
+    return "";
   }
 }
