c45ff9289fc73a5b12563eebac52cbf5a4f7fead
Michael <mike@romacode.com>
2021-11-27 11:20:58 -0500

    fix utc date time pipe to handle empty dates

--- src/app/common/pipes/utc-date-time.pipe.ts
+++ src/app/common/pipes/utc-date-time.pipe.ts
@@ -7,11 +7,13 @@ import { DatePipe } from '@angular/common';
 export class UtcDateTimePipe implements PipeTransform {
 
   // inject date pipe
-  constructor(private datePipe: DatePipe) {}
-  
+  constructor(private datePipe: DatePipe) { }
+
   // transform
-  transform(value: any, format: string): string {    
-    if(value) {
+  transform(value: any, format: string): string {
+
+    // check if a value
+    if (value && value !== "1900-01-01T00:00:00") {
       return this.datePipe.transform(new Date(value + 'Z'), format);
     }
     return '';
