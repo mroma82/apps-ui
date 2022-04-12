153477e4ee9fbde360467312d41799f7ba84fba7
Michael Roma <mike@romacode.com>
2022-04-12 12:29:51 -0400

    fix from rebase

--- src/app/apps/pm/services/pm-lists.service.ts
+++ src/app/apps/pm/services/pm-lists.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { mergeMap, take } from 'rxjs/operators';
 import { AuthService } from 'src/app/common/services/auth.service';
 import { ListItemService } from 'src/app/common/services/list-item.service';
+import { GlobalListsService } from '../../../core/services/global-lists.service';
 import { PmService } from './pm.service';
 
 
@@ -10,30 +11,30 @@ import { PmService } from './pm.service';
 export class PmListsService {
 
   // observables
-  userList$ : Observable<any>;
-  itemTypeList$ : Observable<any>;
-  locationList$ : Observable<any>;
+  userList$: Observable<any>;
+  itemTypeList$: Observable<any>;
+  locationList$: Observable<any>;
 
   // new
   constructor(
-    authService : AuthService,
-    service : PmService,
+    globalLists: GlobalListsService,
+    service: PmService,
     listItems: ListItemService
-  ) { 
+  ) {
 
     // set lists
-    this.userList$ = authService.getUsers();
+    this.userList$ = globalLists.getUsers();
 
     // get params    
     const params$ = service.getParameters();
 
     // get item type from params
-    this.itemTypeList$ = params$.pipe(mergeMap((x : any) => 
+    this.itemTypeList$ = params$.pipe(mergeMap((x: any) =>
       listItems.getItemsByType(x.itemTypeListTypeId).pipe(take(1))
     ));
 
     // get location from params    
-    this.locationList$ = params$.pipe(mergeMap((x : any) => 
+    this.locationList$ = params$.pipe(mergeMap((x: any) =>
       listItems.getItemsByType(x.locationListTypeId).pipe(take(1))
     ));
   }
