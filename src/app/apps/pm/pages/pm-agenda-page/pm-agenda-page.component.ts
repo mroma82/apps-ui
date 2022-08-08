13afeafaf9ca64b604a698a6ee9fc88ec59df157
Michael Roma <mike@romacode.com>
2022-08-08 16:54:43 -0400

    pm progress

---
+++ src/app/apps/pm/pages/pm-agenda-page/pm-agenda-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pm-agenda-page',
+  templateUrl: './pm-agenda-page.component.html',
+  styleUrls: ['./pm-agenda-page.component.sass']
+})
+export class PmAgendaPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
